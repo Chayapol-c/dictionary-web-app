c7ade6683e88dfaedede00edc4e3188d5218ef31
Chayapol Chaipongsawalee <chayapol.c@arise.tech>
2024-11-20 15:13:28 +0700

    feat: add dictionary get word info service

---
+++ src/app/dictionary.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Dictionary} from './dictionary';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DictionaryService {
+
+  baseUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+
+  constructor(private http: HttpClient) { }
+
+  getWordInfo(word: string): Observable<Dictionary[]> {
+    return this.http.get<Dictionary[]>(this.baseUrl + word)
+  }
+}
