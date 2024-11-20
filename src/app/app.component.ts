1e0af3107672d7154264a29d28d04632abc9e551
Chayapol Chaipongsawalee <chayapol.c@arise.tech>
2024-11-20 15:14:45 +0700

    feat: impl dictionary api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {DictionaryService} from './dictionary.service';
+import {Dictionary} from './dictionary';
+import {Observable} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dictionary-web-app';
+  word = ""
+
+  searchedWordInfo$!: Observable<Dictionary[]>
+
+  constructor(private dictionaryService: DictionaryService) {}
+
+
+  onSearch() {
+    if (this.word == "") {
+      return
+    }
+    this.searchedWordInfo$ = this.dictionaryService.getWordInfo(this.word)
+  }
 }
